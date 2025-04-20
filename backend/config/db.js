652ef505c8f48bc009accb60c2244cdaa32eecc4
Muhammad Zubair Khan <157364979+SlippinZubair007@users.noreply.github.com>
2025-04-20 18:57:41 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); 
 const sql = require('mssql');
 
 console.log("Loaded ENV variables:");
 console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? '***' : undefined); // don't log the full password
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? '***' : undefined); 
 console.log("DB_SERVER:", process.env.DB_SERVER);
 console.log("DB_NAME:", process.env.DB_NAME);
 
@@ -25,7 +25,7 @@ const dbConfig = {
 const poolPromise = new sql.ConnectionPool(dbConfig)
   .connect()
   .then(pool => {
-    console.log("DB_SERVER from .env:", process.env.DB_SERVER);  // should print localhost\SQLEXPRESS
+    console.log("DB_SERVER from .env:", process.env.DB_SERVER); 
     console.log('Connected to MSSQL');                  
     return pool;
   })
